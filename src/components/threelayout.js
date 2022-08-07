808a27bc1b4e5e423e4e74f13960ceb02aee64de
rencewang <47406212+rencewang@users.noreply.github.com>
2022-08-07 15:59:35 -0700

    greeting as state

--- src/components/threelayout.js
+++ src/components/threelayout.js
@@ -66,6 +66,53 @@ const Clock = () => {
   return <div id="clock">{time.toLocaleTimeString()}</div>;
 };
 
+const greetingArray = [
+  'The waves come after midnight.',
+  'Fall asleep to revolution, then wake up next to a sad excuse.',
+  "Isn't it pretty to think so?",
+  'The lies I weave are all so intricate.',
+  "Don't you know there's a part of me that longs to go?",
+  "Why settle for less when there's more?",
+  'To be honest, what I liked, were the things I didn’t know.',
+  'Pray for everything we lost, buy back the secrets.',
+  'Would you believe me now, if I say I got caught up in a wave?',
+  'So I let it happen again, almost gave it away.',
+  "I'm still not sure if fear's a rival or close relative to truth.",
+  "There's no going back against this California feeling.",
+  "You win some and lose some, 'long as the outcome's income.",
+  'I want it all, and then some.',
+  "Either way, we're not alone. I'll find a new place to be from.",
+  'On a hard dry surface, you have to engrave the truth.',
+  "Look at the world, so close, and I'm halfway to it.",
+  'But now, more will go with age.',
+  'You are what you love, not who loves you.',
+  'In your darkest hours you will stumble on a secret power.',
+  'Some days I run for cover some I only run my mouth.',
+  'Try to shine. Stay wild.',
+  'When it rains it pours, so stay thirsty like before.',
+  'It’s a train wreck, but I won’t crash it.',
+  'Turn around and you’ll see what I see.',
+  "If it's all in my head tell me now.",
+  'It is no bad thing to celebrate a simple life.',
+  'How long will it be cute?',
+  'It strengthens me to know the truth at last.',
+  'Criticize a little something because somebody has to.',
+  'Am I extreme?',
+  'But it frightens me, the thought against my chest.',
+  'This love will come back to me.',
+  "If everything is not fine in the end, then it's not the end.",
+  "You miss all the shots you don't take.",
+  'He who has a why can bear almost any how.',
+  'Walking through a lot of rainstorms makes one clean.',
+  "The world will break your heart ten ways to Sunday. That's guaranteed.",
+  "Most things don't exist.",
+  'There in the rear-view mirror disappearing now.',
+  "Now I'm dodging plot holes in my sunburnt Silverado.",
+  'Are you happy?',
+  'You should at least have something for that.',
+  "But that's impossible.",
+];
+
 // Main render function
 const Layout = ({ children }) => {
   // checking broswer size for three.js zoom
@@ -75,54 +122,9 @@ const Layout = ({ children }) => {
     fov = window.innerWidth > 500 ? 40 : 75;
   }
 
-  const greetingArray = [
-    'The waves come after midnight.',
-    'Fall asleep to revolution, then wake up next to a sad excuse.',
-    "Isn't it pretty to think so?",
-    'The lies I weave are all so intricate.',
-    "Don't you know there's a part of me that longs to go?",
-    "Why settle for less when there's more?",
-    'To be honest, what I liked, were the things I didn’t know.',
-    'Pray for everything we lost, buy back the secrets.',
-    'Would you believe me now, if I say I got caught up in a wave?',
-    'So I let it happen again, almost gave it away.',
-    "I'm still not sure if fear's a rival or close relative to truth.",
-    "There's no going back against this California feeling.",
-    "You win some and lose some, 'long as the outcome's income.",
-    'I want it all, and then some.',
-    "Either way, we're not alone. I'll find a new place to be from.",
-    'On a hard dry surface, you have to engrave the truth.',
-    "Look at the world, so close, and I'm halfway to it.",
-    'But now, more will go with age.',
-    'You are what you love, not who loves you.',
-    'In your darkest hours you will stumble on a secret power.',
-    'Some days I run for cover some I only run my mouth.',
-    'Try to shine. Stay wild.',
-    'When it rains it pours, so stay thirsty like before.',
-    'It’s a train wreck, but I won’t crash it.',
-    'Turn around and you’ll see what I see.',
-    "If it's all in my head tell me now.",
-    'It is no bad thing to celebrate a simple life.',
-    'How long will it be cute?',
-    'It strengthens me to know the truth at last.',
-    'Criticize a little something because somebody has to.',
-    'Am I extreme?',
-    'But it frightens me, the thought against my chest.',
-    'This love will come back to me.',
-    "If everything is not fine in the end, then it's not the end.",
-    "You miss all the shots you don't take.",
-    'He who has a why can bear almost any how.',
-    'Walking through a lot of rainstorms makes one clean.',
-    "The world will break your heart ten ways to Sunday. That's guaranteed.",
-    "Most things don't exist.",
-    'There in the rear-view mirror disappearing now.',
-    "Now I'm dodging plot holes in my sunburnt Silverado.",
-    'Are you happy?',
-    'You should at least have something for that.',
-    "But that's impossible.",
-  ];
-  let greetingText =
-    greetingArray[Math.floor(Math.random() * greetingArray.length)];
+  const [greetingText] = useState(
+    greetingArray[Math.floor(Math.random() * greetingArray.length)]
+  );
 
   return (
     <main>
