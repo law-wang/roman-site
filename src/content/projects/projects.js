d13716a6c57f749ab53c68829020cc40508aff97
rencewang <47406212+rencewang@users.noreply.github.com>
2023-02-07 22:29:44 -0500

    update project descriptions

--- src/content/projects/projects.js
+++ src/content/projects/projects.js
@@ -3,77 +3,82 @@ const projects = [
     title: 'Thoughts',
     year: '2019-Present',
     description:
-      'Type-foundry-esque platform for logging daily thoughts, powered by AWS Amplify',
+      'Type-foundry-esque platform for logging daily thoughts, monthly playlists, inspirational quotes, and spontaneous audio. Powered by AWS Amplify.',
     link: 'https://thoughts.rence.la/',
   },
   {
     title: 'Spotlist Analysis',
     year: '2022',
     description:
-      'Expansion on Spotlist that offers insights into user created playlists, such as genre and audio features, using Next.js',
+      'Expansion on Spotlist that offers insights into user created playlists, such as genre, history, and audio features. Built with Next.js. [Under Development].',
     link: 'https://spot.rence.la/',
   },
   {
     title: 'Remember',
     year: '2022',
-    description: 'Things that we should remember',
+    description:
+      "Things that we should remember even as we get older. Hopefully it'll provide courage for you to fight another day. [Under Development].",
     link: 'https://remember.rence.la/',
   },
   {
     title: "Media's Role in American Public Opinion of China",
     year: '2022',
-    description: 'Project for Media and Democracy, Fall 2022',
+    description:
+      'Final project for PLSC 247: Media and Democracy, Fall 2022 Yale College. Research on how coverage of several key issues about China led US public opinion.',
     link: 'https://media.rence.la/',
   },
   {
     title: 'Electoral System in the P.R.C.',
     year: '2022',
     description:
-      'Project for Comparative Political Parties and Electoral Systems, Fall 2022',
+      'Project for PLSC 358: Comparative Political Parties and Electoral Systems, Fall 2022 Yale College. Overview of how the electoral system in the P.R.C. is supposed to work, at least on paper.',
     link: 'https://prc.rence.la/',
   },
   {
-    title: 'Collection Chinese Idioms',
+    title: 'Chinese Idioms',
     year: '2022',
-    description: 'fdsa',
+    description:
+      'Presenting some of my favorite Chinese idioms with infinite scroll, SVG stretch animation, and a dictionary. [Under Development].',
     link: 'https://idioms.rence.la/',
   },
   {
     title: 'Spotlist',
     year: '2020-2021',
     description:
-      'Retrieve information about any Spotify playlist, built with React and Spotify API',
+      'Retrieve information about any Spotify playlist because Spotify makes it really hard to copy song names and I cannot type Korean. Built with React and Spotify API',
     link: 'https://spotlist.netlify.app/',
-  },
-  {
-    title: 'In My Defense',
-    year: '2019-2021',
-    description: 'poetry',
-    link: 'https://inmydefense.glitch.me/',
   },
   {
     title: 'Planned Universe',
     year: '2020',
-    description: 'Blog for collecting floor plans built with Gatsby',
+    description: 'Blog for collecting floor plans. Built with Gatsby',
     link: 'https://universe.rence.la/',
+  },
+  {
+    title: 'In My Defense',
+    year: '2019',
+    description:
+      'Interactive poetry project that is actually a bunch of song lyrics pieced together.',
+    link: 'https://inmydefense.glitch.me/',
   },
   {
     title: '10 Albums of 2010s',
     year: '2019',
     description:
-      'Showcase of book-binding project for ART 006: Art of the Printed Word, brought online using the Last.fm API',
+      'Showcase of book-binding project for ART 006: Art of the Printed Word, Fall 2019 Yale College. Brought online using the Last.fm API',
     link: 'https://10of10s.netlify.app/',
   },
   {
     title: 'Breathe',
     year: '2019',
-    description: 'Minimalist new page extension that reminds one to breathe',
+    description: 'Minimalist new page extension that reminds you to breathe.',
     link: 'https://chrome.google.com/webstore/detail/breathe-minimalist-new-ta/ngcbnnpkcfomilobdpffbkcabmehjnbm',
   },
   {
     title: 'The Politic Hopefuls',
     year: '2019',
-    description: 'publica',
+    description:
+      "Minisite for the Yale Politic's special coverage of some 2020 election candidates.",
     link: 'https://chiaski.github.io/politic-hopefuls/',
   },
 ];
